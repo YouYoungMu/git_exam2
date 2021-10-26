7de9f0a798d5aacd51b16751832db708bf553886
You, Youngmu <dbdudann99@naver.com>
2021-10-26 09:36:05 +0900

    201734020-3

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -3,6 +3,7 @@ package git_exam2;
 public class Hello {
 
 	public static void main(String[] args) {
+		System.out.println("IT융합자율학부");
 		System.out.println("201734020");
 		System.out.println("유영무");
 	}
